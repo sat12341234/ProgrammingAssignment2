fa72e343983dcc7099491483b21f46c972ab58c6
sat12341234 <satishma@hotmail.com>
2016-03-06 20:17:01 -0700

    Implementation of assignment functions
    Updated file with implementation for makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file describes 2 functions for the course assignment
+## makeCacheMatrix creates a group of functions 
+##    get, getmatrix, set and setmatrix
+## cacheSolve of the inverted matrix if "m" is NULL
+## Ths implementation is identical to the mean example, I have used solve in place of mean.
+##
 
-## Write a short comment describing this function
+####################################
+## tested with following inputs
+#> x <- matrix(1:4, 2, 2)
+#> x
+#     [,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+#> solve(x)
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+####################################
+
+####################################
+## TEST WITH OUR FUNCTIONS
+#> m <- makeCacheMatrix()
+#> m$set(matrix(1:4, 2, 2))
+#> cacheSolve(m)
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#> cacheSolve(m)
+#getting cached data
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+####################################
+
 
 makeCacheMatrix <- function(x = matrix()) {
+
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 
 }
 
@@ -12,4 +55,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmatrix(m)
+        m
 }
